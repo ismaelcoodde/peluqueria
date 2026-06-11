709b03f363df4a90e5a14be039f0dbd15915906f
Ismael Cruz <ibcruzdeveloper@gmail.com>
2026-06-12 01:33:15 +0200

    borro todo el frontend empiezo de 0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,7 @@
-import { Routes, Route } from 'react-router-dom'
-import Admin from './pages/Admin'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Servicios from './components/Servicios'
-import Reservas from './components/Reservas'
-import Footer from './components/Footer'
-
-
+import Navbar from "./componentes/Navbar"
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={
-        <div>
-          <Navbar />
-          <Hero />
-          <Servicios />
-          <Reservas />
-          <Footer />
-        </div>
-      } />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
-  )
+  return <Navbar/>
 }
 
 export default App
---
+++ frontend/src/componentes/Navbar.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+function Navbar() {
+  const [menuAbierto, setMenuAbierto] = useState(false);
+
+  return (
+    <nav className="bg-gray-900 text-white p-5 fixed top-0 left-0 right-0 z-50">
+      <div className="flex justify-between">
+        <div>Logo</div>
+        <div className="gap-4 hidden md:flex">
+          <a className="hover:text-gray-300 transition  duration-500" href="">
+            Inicio
+          </a>
+          <a className="hover:text-gray-300 transition duration-500" href="">
+            Servicios
+          </a> 
+          <a className="hover:text-gray-300 transition duration-500" href="">
+            Reservas
+          </a>
+          <a className="hover:text-gray-300 transition duration-500" href="">
+            Contacto
+          </a>
+        </div>
+        <button
+          onClick={() => setMenuAbierto(!menuAbierto)}
+          className="md:hidden"
+        >
+          {menuAbierto ? "X" : "☰"}
+        </button>
+      </div>
+
+      
+        <div className={`${menuAbierto ? "max-h-96" : "max-h-0"} flex flex-col md:hidden overflow-hidden transition-all duration-400`}>
+          <a className="hover:text-gray-300 transition duration-5000 block py-3 border-b border-gray-700 text-center" href="" onClick={() => setMenuAbierto(false)}>
+            Inicio
+          </a>
+          <a className="hover:text-gray-300 transition duration-500 block py-3 border-b border-gray-700 text-center" href="" onClick={() => setMenuAbierto(false)}>
+            Servicios
+          </a>
+          <a className="hover:text-gray-300 transition duration-500 block py-3 border-b border-gray-700 text-center" href="" onClick={() => setMenuAbierto(false)}>
+            Reservas
+          </a>
+          <a className="hover:text-gray-300 transition duration-500 block py-3 border-b border-gray-700 text-center" href="" onClick={() => setMenuAbierto(false)}>
+            Contacto
+          </a>
+        </div>
+      
+    </nav>
+  );
+}
+
+export default Navbar;
