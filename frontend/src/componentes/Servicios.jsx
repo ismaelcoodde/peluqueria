a6e41d787a3ad478b6ff68c4708539b931a52c55
Ismael Cruz <ibcruzdeveloper@gmail.com>
2026-06-12 18:59:57 +0200

    añado 3 targetas de servicios

--- frontend/src/componentes/Servicios.jsx
+++ frontend/src/componentes/Servicios.jsx
@@ -1,9 +1,30 @@
 function Servicios() {
   return (
     <section className="bg-black text-white py-20 px-8">
-      <h1 className="text-center text-4xl font-bold mb-12">Nuestros Servicios</h1>
-      <div>
-        <p>hol</p>
+      <h1 className="text-center text-4xl font-bold mb-12">
+        Nuestros Servicios
+      </h1>
+      <div className="flex flex-col md:flex-row gap-8">
+        <div className="border border-gray-700 border-t-2 border-t-red-500 hover:border-t-white hover:scale-105 transition-all duration-300 p-8 flex-1">
+          <h2 className="text-2xl font-bold mb-4">Corte + Barba</h2>
+          <p>15 minutos</p>
+          <p className="text-red-500 font-bold mt-4">18€</p>
+        </div>
+      
+            
+        <div className="border border-gray-700 border-t-2 border-t-red-500 hover:border-t-white hover:scale-105 transition-all duration-300 p-8 flex-1">
+          <h2 className="text-2xl font-bold mb-4">Corte Señora</h2>
+          <p>30 minutos</p>
+          <p className="text-red-500 font-bold mt-4">25€</p>
+        </div>
+      
+           
+        <div className="border border-gray-700 border-t-2 border-t-red-500 hover:border-t-white hover:scale-105 transition-all duration-300 p-8 flex-1">
+          <h2 className="text-2xl font-bold mb-4">Tinte</h2>
+          <p>45 minutos</p>
+          <p className="text-red-500 font-bold mt-4">40€</p>
+        
+      </div>
       </div>
     </section>
   );
