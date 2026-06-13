1b7315ebc9eedfda24a92ac0809a897bfbb98f69
Ismael <ibcruzdeveloper@gmail.com>
2026-06-13 20:52:52 +0200

    fadeinup al hero

--- frontend/src/componentes/Hero.jsx
+++ frontend/src/componentes/Hero.jsx
@@ -10,7 +10,7 @@ function Hero() {
         playsInline
       ></video>
       <div className="bg-black absolute inset-0 opacity-60"></div>
-      <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+      <div className="absolute inset-0 flex flex-col items-center justify-center text-white animate-fadeInUp">
         <h1 className="text-5xl font-bold mb-2">Barber Ismael</h1>
         <p className="text-gray-300 text-1xl">Te corto el pelo a puñetazos</p>
         <a href="#reservas" className="border border-white px-8 py-3 mt-8 bg-black/30 hover:bg-white hover:text-black transition duration-500">Reservar Cita</a>
